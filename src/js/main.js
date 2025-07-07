dd7bd54722a3d57d071ea8b0dc441866e3a27bb9
Ava Mandoli <avamandoli@gmail.com>
2025-07-07 14:29:01 -0700

    cleaning up; fixed starting jumpiness issue

--- src/js/main.js
+++ src/js/main.js
@@ -21,28 +21,26 @@ function horizontalScroller(){
     //These vars help us determine how long our invisible scroll container needs to be for us to see the whole graphic on any screen
   let windowWidth = window.innerWidth;
   let horizontalLength = document.querySelector(".graphic-wrapper").scrollWidth; //  how wide the graphic is
-
-  console.log("horizontalLength of graphic-wrapper:", horizontalLength)
-
   let distFromTop = document.querySelector(".horizontal-section").offsetTop; // how far the horizontal section is from page top
 
-  console.log("horizontal section distFromTop:", distFromTop)
-
   // Dynamically calculate scroll distance based on viewport width (smaller screen width means you have to scroll more to see whole graphic. Must subtract windowWidth, otherwise the graphic will scroll all the way off the page and you'll see white. We want to stop when right edge of graphic reaches right edge of viewport.)
-  let scrollLength = (horizontalLength *1.1) - windowWidth // length of graphic
+  let scrollLength = (horizontalLength *1.05) - windowWidth // length of graphic
   let scrollDistance = distFromTop +  scrollLength; // length from top + graphic 
   let scrollHeight = scrollLength +window.innerHeight;
 
  document.querySelector(".horizontal-section").style.height = scrollHeight  +  "px" 
-  // console.log("scrollyContainerHeight needed:", scrollyContainerHeight)
 
   window.onscroll = function(){
   let scrollTop = window.pageYOffset
-  console.log("window Y offset is:", scrollTop)
-  // As soon as we have scrolled down (scrollTop) to where the graphic is (distFromTop), start moving the graphic-wrapper left. Stop moving it left once we have reached the end of the graphic (ie the end of scrolldistance)
-  if (scrollTop >= distFromTop && scrollTop < scrollDistance){
+  let graphicScroller = document.querySelector(".graphic-wrapper")
+
+    // As soon as we have scrolled down (scrollTop) to where the graphic is (distFromTop), start moving the graphic-wrapper left. Stop moving it left once we have reached the end of the graphic (ie the end of scrolldistance)
+  if (scrollTop < distFromTop) {
+      graphicScroller.style.transform = "translateX(0px)";
+  } else if (scrollTop >= distFromTop && scrollTop < scrollDistance){
     document.querySelector(".graphic-wrapper").style.transform= "translateX(-"+(scrollTop-distFromTop)+"px)";
   }
+
 }
 }
 
